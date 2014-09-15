4c07c34e42ba41d6a3609886e3234de16edcb669
aditya <adityapranav@users.noreply.github.com>
2014-09-15 12:22:37 +0530

    Update FindMissingArray.c

--- FindMissingArray.c
+++ FindMissingArray.c
@@ -4,18 +4,9 @@ int findMissingElement(int a[],int size)
 {
   int i;
   int temp = 0;
-  for( i=1; i<=size ; i++ )
-  {
-    temp = temp^i^a[i];   
-  }
-  return temp;
-  /* or we can do this also 
-  int i;
-  int temp = 0;
   for( i=0; i<size ; i++ )
   {
     temp = temp^i^a[i];   
   }
   return temp^i;
-  */
 }
