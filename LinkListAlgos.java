f010cbc4e9771c8bfeceffb6f2bcf7399aabc65a
aditya <adityapranav@users.noreply.github.com>
2014-09-21 08:31:34 +0530

    Create LinkListAlgos.java

---
+++ LinkListAlgos.java
@@ -0,0 +1,27 @@
+// This is a subclass of LinkedList which has some of the most important Algorithms implemented.
+public LinkListAlgos<T> extends LinkedList<T>
+{
+
+  public boolean isSizeOdd( )
+  {
+
+    ListNode<T> current = header;
+    
+    byte flag = 0;
+    
+    while( current )
+    {
+      flag != flag;
+      
+      current=current.next;
+    }
+
+    if( flag == 0)
+       return true;
+       
+    return false;
+    
+  }
+
+
+}
