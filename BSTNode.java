c10a9f0dbeafd2f24ed8a8e592d630d313896642
aditya <adityapranav@users.noreply.github.com>
2014-10-12 20:55:43 +0530

    Create BSTNode.java

---
+++ BSTNode.java
@@ -0,0 +1,11 @@
+package com.ds.bst;  
+class BSTNode<T> 
+{    
+  public BSTNode left;    
+  public BSTNode right;    
+  public T data;        
+  public BSTNode(T data)    
+  { 	   
+    this.data = data;    
+  } 
+}
