11d319a572d87ccb76f1b18a1a072d7ec634351c
aditya <adityapranav@users.noreply.github.com>
2015-01-30 14:48:47 +0530

    Update Sortings.java

--- Sortings.java
+++ Sortings.java
@@ -1,3 +1,5 @@
+package com.ds.sort;
+
 public abstract class Sortings
 {
   public static void insertionSort(int[] a)
@@ -9,11 +11,30 @@ public abstract class Sortings
         while( i >=0 && a[i] > key )
         {
           a[i+1] = a[i];
+          --i;
         }
         a[i+1] = key;
       }
   }
   
-  public static void mergeSort(int[] a);
+  public abstract void mergeSort(int[] a);
   
-}
+  public static void selectionSort(int[] a)
+  {
+    for(int i=0; i < a.length; ++i)
+    {
+      int pos = i;
+      for(int j=i+1; j < a.length-1; ++j)
+      {
+        if(a[j] < a[pos])
+        {
+          pos = j;
+        }
+      }
+      int temp=a[i];
+      a[i] = a[pos];
+      a[pos] = temp;
+      
+    }
+  }
+ 
